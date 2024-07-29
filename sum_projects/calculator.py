7da4e5fc0f573ef4884933b767587de6dbb22f1c
u38952zl <zi.lim-4@student.manchester.ac.uk>
2024-07-29 17:39:32 +0800

    oops

---
+++ sum_projects/calculator.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+def buttonpress(num):
+    pass
+
+def equal():
+    pass
+
+def clear():
+    pass 
+
+window = Tk()
+window.title("Calculator program")
+window.geometry("500x500")
+
+equation_text = ""
+
+equation_label = StringVar()
+
+label = Label(window, textvariable=equation_label, font=('consolas',20), bg="white", width=24, height=2)
+label.pack()
+
+frame = Frame(window)
+frame.pack()
+
+
+window.mainloop()
